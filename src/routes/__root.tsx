252ef42696cacf50a3c7303a73708926b8e257e6
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-10-01 20:36:02 +0200

    cleanup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -8,8 +8,6 @@ import { TanstackDevtools } from '@tanstack/react-devtools'
 
 import Header from '../components/Header'
 
-import StoreDevtools from '../lib/demo-store-devtools'
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
 
 import appCss from '../styles.css?url'
@@ -68,7 +66,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
               name: 'Tanstack Router',
               render: <TanStackRouterDevtoolsPanel />,
             },
-            StoreDevtools,
             TanStackQueryDevtools,
           ]}
         />
