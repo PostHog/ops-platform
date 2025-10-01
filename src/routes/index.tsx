689a83b65938ff921986981bfa632fef87f52223
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-10-01 21:38:21 +0200

    Revert "fix"
    This reverts commit 78140dcadb802ee79ad005d825ad65ee89e0a96c.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -33,7 +33,7 @@ import {
 } from "@/components/ui/table"
 import { createServerFn } from '@tanstack/react-start'
 import prisma from '@/db'
-import { Prisma } from "@prisma/client";
+import { Prisma } from "@/../generated/prisma/client.js";
 
 export const Route = createFileRoute("/")({
   component: App,
