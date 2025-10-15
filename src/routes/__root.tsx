266140698fb9266b90749dd9ac3615c197f5a2ae
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-10-15 12:40:19 +0200

    cleanup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -14,13 +14,8 @@ import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
 
-import type { TRPCRouter } from '@/integrations/trpc/router'
-import type { TRPCOptionsProxy } from '@trpc/tanstack-react-query'
-
 interface MyRouterContext {
   queryClient: QueryClient
-
-  trpc: TRPCOptionsProxy<TRPCRouter>
 }
 
 export const Route = createRootRouteWithContext<MyRouterContext>()({
