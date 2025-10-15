2ec0ac29852b06720701661e78165437f16099af
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-10-15 11:56:09 +0200

    add filtering

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ import {
   getSortedRowModel,
   SortingState,
 } from '@tanstack/react-table'
-import type { ColumnDef } from '@tanstack/react-table'
+import type { Column, ColumnDef, Row, RowData } from '@tanstack/react-table'
 import { ChevronDown, MoreHorizontal } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import {
@@ -48,6 +48,14 @@ type Employee = Prisma.EmployeeGetPayload<{
   }
 }>
 
+declare module '@tanstack/react-table' {
+  //allows us to define custom properties for our columns
+  interface ColumnMeta<TData extends RowData, TValue> {
+    filterVariant?: 'text' | 'range' | 'select' | 'dateRange'
+    filterOptions?: { label: string, value: string }[]
+  }
+}
+
 export const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 export const getEmployees = createServerFn({
@@ -67,6 +75,36 @@ export const getEmployees = createServerFn({
   })
 })
 
+const customFilterFns = {
+  inDateRange: (row: Row<Employee>, _: string, filterValue: [string | undefined, string | undefined]) => {
+    const [minStr, maxStr] = filterValue as [string | undefined, string | undefined]
+    const date = new Date(row.original.salaries?.[0]?.timestamp)
+    const value = date.getTime()
+
+    const min = minStr ? new Date(minStr).getTime() : undefined
+    const max = maxStr ? new Date(maxStr).getTime() : undefined
+
+    if (min !== undefined && value < min) return false
+    if (max !== undefined && value > max) return false
+    return true
+  },
+  inNumberRange: (value: number, _: string, filterValue: [number | undefined, number | undefined]) => {
+    const [min, max] = filterValue as [number | '', number | '']
+
+    if (min !== '' && value < min) return false
+    if (max !== '' && value > max) return false
+    return true
+  },
+  containsText: (value: string, _: string, filterValue: string) => {
+    if (filterValue !== '' && !value.includes(filterValue)) return false
+    return true
+  },
+  equals: (value: string, _: string, filterValue: string) => {
+    if (filterValue !== '' && value !== filterValue) return false
+    return true
+  }
+}
+
 function App() {
   const router = useRouter()
   const [sorting, setSorting] = React.useState<SortingState>([])
@@ -85,6 +123,9 @@ function App() {
     {
       accessorKey: "name",
       header: "Name",
+      meta: {
+        filterVariant: 'text',
+      },
       cell: ({ row }) => (
         <div>{row.original.name}</div>
       ),
@@ -92,6 +133,11 @@ function App() {
     {
       accessorKey: "timestamp",
       header: "Last Change (date)",
+      meta: {
+        filterVariant: 'dateRange',
+      },
+      filterFn: customFilterFns.inDateRange,
+      enableColumnFilter: true,
       cell: ({ row }) => {
         const date = new Date(row.original.salaries?.[0]?.timestamp)
         return <div>{months[date.getMonth()]} {date.getFullYear()}</div>
@@ -100,46 +146,89 @@ function App() {
     {
       accessorKey: "level",
       header: "Level",
+      meta: {
+        filterVariant: 'range',
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: [number | undefined, number | undefined])=> customFilterFns.inNumberRange(row.original.salaries?.[0]?.level, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].level}</div>,
     },
     {
       accessorKey: "step",
       header: "Step",
+      meta: {
+        filterVariant: 'range',
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: [number | undefined, number | undefined])=> customFilterFns.inNumberRange(row.original.salaries?.[0]?.step, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].step}</div>,
     },
     {
       accessorKey: "totalSalary",
       header: "Total Salary",
+      meta: {
+        filterVariant: 'range',
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: [number | undefined, number | undefined])=> customFilterFns.inNumberRange(row.original.salaries?.[0]?.totalSalary, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].totalSalary}</div>,
     },
     {
       accessorKey: "changePercentage",
       header: "Last Change (%)",
+      meta: {
+        filterVariant: 'range',
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: [number | undefined, number | undefined])=> customFilterFns.inNumberRange(row.original.salaries?.[0]?.changePercentage * 100, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].changePercentage * 100}%</div>,
     },
     {
       accessorKey: "changeAmount",
       header: "Last Change ($)",
+      meta: {
+        filterVariant: 'range',
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: [number | undefined, number | undefined])=> customFilterFns.inNumberRange(row.original.salaries?.[0]?.changeAmount, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].changeAmount}</div>,
     },
     {
       accessorKey: "priority",
       header: "Priority",
+      meta: {
+        filterVariant: 'select',
+        filterOptions: [
+          { label: 'Low', value: 'low' },
+          { label: 'Medium', value: 'medium' },
+          { label: 'High', value: 'high' },
+        ]
+      },
       cell: ({ row }) => <div>{row.original.priority}</div>,
     },
     {
       accessorKey: "reviewer",
       header: "Reviewer",
+      meta: {
+        filterVariant: 'text',
+      },
       cell: ({ row }) => <div>{row.original.reviewer}</div>,
     },
     {
       accessorKey: "notes",
       header: "Notes",
+      meta: {
+        filterVariant: 'text'
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: string)=> customFilterFns.containsText(row.original.salaries?.[0]?.notes, _, filterValue),
       cell: ({ row }) => <div>{row.original.salaries[0].notes}</div>,
     },
     {
       accessorKey: "reviewd",
       header: "Reviewd",
+      meta: {
+        filterVariant: 'select',
+        filterOptions: [
+          { label: 'Yes', value: 'true' },
+          { label: 'No', value: 'false' },
+        ]
+      },
+      filterFn: (row: Row<Employee>, _: string, filterValue: string)=> customFilterFns.equals(row.original.reviewd.toString(), _, filterValue),
       cell: ({ row }) => <div>{row.original.reviewd ? "Yes" : "No"}</div>,
     },
     {
@@ -184,9 +273,7 @@ function App() {
       columnFilters,
       columnVisibility,
     },
-    filterFns: {
-      fuzzy: () => true,
-    },
+    filterFns: {},
   })
 
   return (
@@ -237,6 +324,11 @@ function App() {
                             header.column.columnDef.header,
                             header.getContext()
                           )}
+                        {header.column.getCanFilter() ? (
+                          <div>
+                            <Filter column={header.column} />
+                          </div>
+                        ) : null}
                       </TableHead>
                     )
                   })}
@@ -294,5 +386,111 @@ function App() {
         </div>
       </div>
     </div>
+  )
+}
+
+function Filter({ column }: { column: Column<any, unknown> }) {
+  const columnFilterValue = column.getFilterValue()
+  const { filterVariant, filterOptions } = column.columnDef.meta ?? {}
+
+  return filterVariant === 'range' ? (
+    <div>
+      <div className="flex space-x-2">
+        {/* See faceted column filters example for min max values functionality */}
+        <DebouncedInput
+          type="number"
+          value={(columnFilterValue as [number, number])?.[0] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [value, old?.[1]])
+          }
+          placeholder={`Min`}
+          className="w-24 border shadow rounded"
+        />
+        <DebouncedInput
+          type="number"
+          value={(columnFilterValue as [number, number])?.[1] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [number, number]) => [old?.[0], value])
+          }
+          placeholder={`Max`}
+          className="w-24 border shadow rounded"
+        />
+      </div>
+      <div className="h-1" />
+    </div>
+  ) : filterVariant === 'dateRange' ? (
+    <div>
+      <div className="flex space-x-2">
+        <DebouncedInput
+          type="date"
+          value={(columnFilterValue as [string, string])?.[0] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [string, string]) => [value, old?.[1]])
+          }
+          placeholder={`Min`}
+          className="w-32 border shadow rounded"
+        />
+        <DebouncedInput
+          type="date"
+          value={(columnFilterValue as [string, string])?.[1] ?? ''}
+          onChange={value =>
+            column.setFilterValue((old: [string, string]) => [old?.[0], value])
+          }
+          placeholder={`Max`}
+          className="w-32 border shadow rounded"
+        />
+      </div>
+      <div className="h-1" />
+    </div>
+  ) : filterVariant === 'select' ? (
+    <select
+      onChange={e => column.setFilterValue(e.target.value)}
+      value={columnFilterValue?.toString()}
+    >
+      {/* See faceted column filters example for dynamic select options */}
+      <option value="">All</option>
+      {filterOptions?.map((option) => (
+        <option key={option.value} value={option.value}>{option.label}</option>
+      ))}
+    </select>
+  ) : (
+    <DebouncedInput
+      className="w-36 border shadow rounded"
+      onChange={value => column.setFilterValue(value)}
+      placeholder={`Search...`}
+      type="text"
+      value={(columnFilterValue ?? '') as string}
+    />
+    // See faceted column filters example for datalist search suggestions
+  )
+}
+
+// A typical debounced input react component
+function DebouncedInput({
+  value: initialValue,
+  onChange,
+  debounce = 500,
+  ...props
+}: {
+  value: string | number
+  onChange: (value: string | number) => void
+  debounce?: number
+} & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'onChange'>) {
+  const [value, setValue] = React.useState(initialValue)
+
+  React.useEffect(() => {
+    setValue(initialValue)
+  }, [initialValue])
+
+  React.useEffect(() => {
+    const timeout = setTimeout(() => {
+      onChange(value)
+    }, debounce)
+
+    return () => clearTimeout(timeout)
+  }, [value])
+
+  return (
+    <input {...props} value={value} onChange={e => setValue(e.target.value)} />
   )
 }
