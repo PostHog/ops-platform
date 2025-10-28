4051fad5da3f6c1dde655fb1a55f3df87f1fbc63
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-10-28 13:35:55 +0100

    fix

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,7 @@
 import { betterAuth } from "better-auth";
 import { admin } from "better-auth/plugins"
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { reactStartCookies } from "better-auth/react-start";
 
 
