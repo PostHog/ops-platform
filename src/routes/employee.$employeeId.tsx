5198dabdca4430cf3edde9b2b4056797751438f4
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-11-06 18:52:39 +0100

    fix

--- src/routes/employee.$employeeId.tsx
+++ src/routes/employee.$employeeId.tsx
@@ -475,6 +475,9 @@ function EmployeeOverview() {
                     ul: ({ children }) => (
                       <ul className="list-disc list-inside">{children}</ul>
                     ),
+                    ol: ({ children }) => (
+                      <ul className="list-decimal list-inside">{children}</ul>
+                    ),
                   }}
                 >
                   {feedback}
