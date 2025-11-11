27f62d231bddc418bad6afc15cc2ceff2f444187
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-11-11 13:23:30 +0100

    fix: set check in feedback flags correctly

---
+++ prisma/migrations/20251111122115_remove_default_from_check_in_feedback_flags/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "Employee" ALTER COLUMN "checkIn30DaysScheduled" DROP DEFAULT,
+ALTER COLUMN "checkIn60DaysScheduled" DROP DEFAULT,
+ALTER COLUMN "checkIn80DaysScheduled" DROP DEFAULT;
