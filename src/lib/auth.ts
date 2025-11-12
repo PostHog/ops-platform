3cb1f5507908e193a6c93ae91052f1c9feb0fa5b
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-11-12 13:38:45 +0100

    feat: prisma accelerate

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,9 @@
 import { betterAuth } from 'better-auth'
 import { admin } from 'better-auth/plugins'
 import { prismaAdapter } from 'better-auth/adapters/prisma'
-import { PrismaClient } from '@prisma/client'
 import { reactStartCookies } from 'better-auth/react-start'
+import prisma from '@/db'
 
-const prisma = new PrismaClient()
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'postgresql',
