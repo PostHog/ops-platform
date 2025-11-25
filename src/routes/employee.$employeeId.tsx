fe6b52d9a2587530181fd9bedc9767358af6a097
MarconLP <13001502+MarconLP@users.noreply.github.com>
2025-11-25 17:53:20 +0100

    test

--- src/routes/employee.$employeeId.tsx
+++ src/routes/employee.$employeeId.tsx
@@ -937,7 +937,7 @@ function EmployeeOverview() {
                     onCheckedChange={setFilterByTitle}
                   />
                   <Label htmlFor="filter-by-title" className="text-sm">
-                    Filter by title
+                    Filter by titles
                   </Label>
                 </div>
               </div>
