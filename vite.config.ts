b065187b069247db382fc7b90fcde4232aa022c8
Marcus Hof <13001502+MarconLP@users.noreply.github.com>
2025-12-01 15:45:57 +0100

    feat: allow configuring allowedHosts through .env (#131)

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,30 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
-const config = defineConfig({
-  plugins: [
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart(),
-    netlify(),
-    viteReact(),
-  ],
-})
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd(), '')
 
-export default config
+  return {
+    plugins: [
+      // this is the plugin that enables path aliases
+      viteTsConfigPaths({
+        projects: ['./tsconfig.json'],
+      }),
+      tailwindcss(),
+      tanstackStart(),
+      netlify(),
+      viteReact(),
+    ],
+    server: {
+      allowedHosts:
+        mode === 'development' && env.VITE_APP_ALLOWED_HOST
+          ? [env.VITE_APP_ALLOWED_HOST]
+          : [],
+    },
+  }
+})
