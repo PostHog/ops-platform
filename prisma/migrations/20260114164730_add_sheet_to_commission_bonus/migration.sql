7367b70b5f6ca2b6a0831f316615e8ece8420dd7
MarconLP <13001502+MarconLP@users.noreply.github.com>
2026-01-15 12:18:03 +0100

    squash migration

--- prisma/migrations/20260114164730_add_sheet_to_commission_bonus/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "CommissionBonus" ADD COLUMN     "sheet" TEXT;
--- prisma/migrations/20260115092049_add_amount_held_to_commission_bonus/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "CommissionBonus" ADD COLUMN     "amountHeld" DOUBLE PRECISION NOT NULL DEFAULT 0;
